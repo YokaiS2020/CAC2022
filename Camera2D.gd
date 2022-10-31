267ff8b6343e41edd7198d676263f57f31deee36
James <James.J.Jiang.1@gmail.com>
2022-10-30 22:46:29 -0700

    Finished Keypad, Gear Puzzle, and End Screen

--- Camera2D.gd
+++ Camera2D.gd
@@ -8,12 +8,14 @@ var S1 = Vector2(515,277)
 var S2 = Vector2(1750,277)
 var SNumber = 1
 var Scenes = [S1,S2]
-
+signal CurrentSceneNumber(Scene)
 var Finished=true
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.position = Scenes[SNumber%2-1]
 func _physics_process(delta):
+	emit_signal("CurrentSceneNumber",SNumber%2)
+
 	if Finished == true:
 		if Input.is_action_just_pressed("Move Scene left"):
 			SNumber-=1
@@ -26,53 +28,3 @@ func _physics_process(delta):
 				
 
 
-func _on_4_mouse_entered():
-	pass # Replace with function body.
-
-
-func _on_5_mouse_entered():
-	pass # Replace with function body.
-
-
-func _on_6_mouse_entered():
-	pass # Replace with function body.
-
-
-func _on_7_mouse_entered():
-	pass # Replace with function body.
-
-
-func _on_8_mouse_entered():
-	pass # Replace with function body.
-
-
-func _on_9_mouse_entered():
-	pass # Replace with function body.
-
-
-func _on_2_mouse_exited():
-	pass # Replace with function body.
-
-
-func _on_3_mouse_exited():
-	pass # Replace with function body.
-
-
-func _on_4_mouse_exited():
-	pass # Replace with function body.
-
-
-func _on_5_mouse_exited():
-	pass # Replace with function body.
-
-
-func _on_6_mouse_exited():
-	pass # Replace with function body.
-
-
-func _on_7_mouse_exited():
-	pass # Replace with function body.
-
-
-func _on_8_mouse_exited():
-	pass # Replace with function body.
