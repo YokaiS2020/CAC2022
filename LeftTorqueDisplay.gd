29f267d59cd46cb75496261bf44ede5ec0e98064
JustinTsai800 <98719229+JustinTsai800@users.noreply.github.com>
2022-11-01 00:23:49 -0700

    finalized the torque readings

--- LeftTorqueDisplay.gd
+++ LeftTorqueDisplay.gd
@@ -1,15 +1,26 @@
 extends RichTextLabel
 
+var can_drop = true
 
+export var drop_rate = 0.5
 # Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
-func _process(delta):
-	set_text("Torque on Left Side: " + str(96))
+var torque = 0
+var added = 0
+var removed = 0
+
 # Called when the node enters the scene tree for the first time.
-
-
+func _process(delta):
+	set_text("Torque on Left Side: " + str(torque))
+	if Input.is_action_just_pressed("Add_Left_Stone") and can_drop:
+		torque += (19.6 * 2.49996)
+		added += 1
+		can_drop = false
+		yield(get_tree().create_timer(drop_rate),"timeout")
+		can_drop = true
+	if Input.is_action_just_pressed("Remove_Left_Stone") and removed < added:
+		torque -= (19.6 * 2.49996)
+		removed += 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
--- RightTorqueDisplay.gd
+++ RightTorqueDisplay.gd
@@ -1,14 +1,25 @@
 extends RichTextLabel
+var can_drop = true
 
-
+export var drop_rate = 0.5
 # Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
+var torque = 98 * 3.18809
+var added = 0
+var removed = 0
 
 # Called when the node enters the scene tree for the first time.
 func _process(delta):
-	set_text("Torque on Right Side: " + str(96))
+	set_text("Torque on Right Side: " + str(torque))
+	if Input.is_action_just_pressed("Add_Right_Stone") and can_drop:
+		torque += (19.6 * 8.87614)
+		added += 1
+		can_drop = false
+		yield(get_tree().create_timer(drop_rate),"timeout")
+		can_drop = true
+	if Input.is_action_just_pressed("Remove_Right_Stone") and removed < added:
+		torque -= (19.6 * 8.87614)
+		removed += 1
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
